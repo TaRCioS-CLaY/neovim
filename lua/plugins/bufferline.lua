760ce7f13d7be87546b92959cb6bd8cbf4655561
Douglas M. <douglasmassolari@hotmail.com>
2026-05-27 18:55:16 +0200

    feat(bufferline): troca o ícone de fechar

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,6 +2,7 @@ vim.pack.add({ "https://github.com/akinsho/bufferline.nvim" })
 
 require("bufferline").setup({
   options = {
+    buffer_close_icon = "×",
     diagnostics = "nvim_lsp",
     diagnostics_indicator = function(_, level)
       local signs = vim.diagnostic.config().signs.text or {}
